1719831fa66a2fdc36bf10b5fc49f0379552beb0
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2026-01-07 16:52:21 -0800

    Phase 4.4 Complete
    Phase 4.4 Complete

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -6,7 +6,7 @@
  *
  * Dashboard Components Index - Export all dashboard components
  * ----------------------------------------------------------------------------
- * FILE VERSION: v5.0-4-4.3-1
+ * FILE VERSION: v5.0-4-4.4-1
  * LAST MODIFIED: 2026-01-07
  * PHASE: Phase 4 - Dashboard & Metrics
  * Repository: https://github.com/the-alphabet-cartel/ash-dash
@@ -16,8 +16,10 @@
 // Metric card component (Step 4.3)
 export { default as MetricCard } from './MetricCard.vue'
 
-// Charts will be added in Steps 4.4 and 4.5
-// export { default as CrisisTrendsChart } from './CrisisTrendsChart.vue'
+// Crisis trends chart (Step 4.4)
+export { default as CrisisTrendsChart } from './CrisisTrendsChart.vue'
+
+// CRT activity chart will be added in Step 4.5
 // export { default as CRTActivityChart } from './CRTActivityChart.vue'
 
 // Active sessions list will be added in Step 4.6
